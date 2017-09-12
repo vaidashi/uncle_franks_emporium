96f3381c795459e64674dd1872b18a5453370c2a
Brandon Stewart <boveus@gmail.com>
2017-09-11 20:47:53 -0600

    Add some style.

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,4 @@
+class TestController < ApplicationController
+  def route
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'test#route'
+get '/', :to => 'test#route', :as => 'test_route'
+
 end
