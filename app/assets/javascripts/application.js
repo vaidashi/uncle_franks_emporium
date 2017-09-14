7a4c78f0b3452184801235a1a82d28dddd7da1a9
Brandon Stewart <boveus@gmail.com>
2017-09-14 10:55:21 -0600

    Add initial mockup of login popup.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,3 +51,13 @@ window.onclick = function(event) {
     }
   }
 }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
