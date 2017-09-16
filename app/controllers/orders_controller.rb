a6025f33934fbcd132fb31d11903121c87b21595
djtrujillo <danny.j.trujillo@gmail.com>
2017-09-16 13:45:25 -0600

    Authenticated user security

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 
 class OrdersController < ApplicationController
+  before_action :require_current_user, only: [:show]
 
   def index
     @orders = Order.where(user_id: current_user.id)
@@ -7,6 +8,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id] )
+    render file: "/public/404" unless (current_user == @order.user)
   end
 
   def create
