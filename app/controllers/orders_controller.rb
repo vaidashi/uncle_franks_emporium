c7235c2389bb2a7787ed27568d9d8311726ee85f
Brandon Stewart <boveus@gmail.com>
2017-09-15 18:48:38 -0600

    Add sad path test.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,9 +11,14 @@ class OrdersController < ApplicationController
 
   def create
     items = @cart.all_items
+    if @cart.contents.empty?
+      flash[:bad_message] = "CART EMPTY FILL IT WITH GARBAGE"
+      redirect_back(fallback_location: root_path)
+    else
     order = Order.create_from_items(items, current_user)
     flash[:good_message] = "Order was successfully placed"
     redirect_to orders_path
+    end
   end
 
 end
--- spec/features/users/visitor_must_be_logged_in_to_checkout_spec.rb
+++ spec/features/users/visitor_must_be_logged_in_to_checkout_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "visitor can create account" do
-  scenario "from login page" do
+  scenario "visitor can create an account and check out" do
     item = create(:item)
 
     visit '/items'
@@ -30,10 +30,33 @@ feature "visitor can create account" do
     order_item = User.first.orders.first.items.first
     user_orders = User.first.orders
     user_order = User.first.orders.first
-    
+
     expect(current_path).to eq(orders_path)
     expect(user_orders.count).to eq(1)
     expect(page).to have_content("Order was successfully placed")
     expect(page).to have_link(user_order.id)
+  end
+  scenario "user can't check out with an empty cart" do
+    visit '/'
+
+    click_on "shopping_cart"
+
+    expect(page).to have_button("To checkout login or create an account")
+
+    click_on "To checkout login or create an account"
+
+    click_on("Create Account", :match => :first)
+
+    fill_in "user[username]", with: "Charlie"
+    fill_in "user[password]", with: "I love the waitress"
+    fill_in "user[password_confirmation]", with: "I love the waitress"
+
+    click_on "Create Account"
+
+    click_on "shopping_cart"
+
+    click_on "Checkout"
+
+    expect(page).to have_content("CART EMPTY FILL IT WITH GARBAGE")
   end
 end
