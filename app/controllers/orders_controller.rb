7319dacc6e013b090b2214a6475a868d0ff5d993
Brandon Stewart <boveus@gmail.com>
2017-09-15 18:43:24 -0600

    Initial test passing.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,4 +9,11 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id] )
   end
 
+  def create
+    items = @cart.all_items
+    order = Order.create_from_items(items, current_user)
+    flash[:good_message] = "Order was successfully placed"
+    redirect_to orders_path
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,15 @@ class Order < ApplicationRecord
   has_many :items, through: :item_orders
   enum status: ["ordered", "paid", "cancelled", "completed"]
 
+  def self.create_from_items(items, current_user)
+    order = Order.create(user_id: current_user.id)
+    items.each do |item, quantity|
+        quantity.times do
+          order.items << item
+        end
+    end
+    order.save
+  end
+
 
 end
