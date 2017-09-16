add576e968d542ae4f7312bfd2e73dc818406d7b
djtrujillo <danny.j.trujillo@gmail.com>
2017-09-16 14:15:45 -0600

    unauthenticated user test

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,13 @@
 
 class OrdersController < ApplicationController
-  before_action :require_current_user, only: [:show]
+  before_action :require_current_user, only: [:index]
 
   def index
-    @orders = Order.where(user_id: current_user.id)
+    if current_user
+      @orders = Order.where(user_id: current_user.id)
+    else
+      redirect_to login_path
+    end
   end
 
   def show
