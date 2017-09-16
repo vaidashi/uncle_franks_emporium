3eda2643d3d4bd54dd5f8c55def444efb9dec86e
Brandon Stewart <boveus@gmail.com>
2017-09-16 15:18:07 -0600

    Add passing feature test for preventing users from accessing admin db.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
   helper_method :current_user
+  helper_method :current_admin?
   before_action :set_new_user
+
+
 
   def set_new_user
     @new_user = User.new
@@ -20,4 +23,7 @@ class ApplicationController < ActionController::Base
     render file: "/public/404" unless current_user
   end
 
+  def current_admin?
+    current_user && current_user.admin?
+  end
 end
--- spec/features/admin/admin_can_visit_their_dashboard_spec.rb
+++ spec/features/admin/admin_can_visit_their_dashboard_spec.rb
@@ -10,4 +10,15 @@ feature "as a logged in admin" do
 
     expect(page).to have_content("Admin Dashboard")
   end
+feature "as a logged in non-admin user" do
+  scenario "they cannot visit the admin dashboard" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/admin/dashboard"
+
+    expect(page).to have_content("The page you were looking for doesn't exist")
+   end
+  end
 end
