187ff6fbb43a9b89ef253a4a4c559dc8eba33140
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-20 13:53:02 -0600

    admin can create item without specifying image

--- spec/features/admin/admin_can_create_an_item_spec.rb
+++ spec/features/admin/admin_can_create_an_item_spec.rb
@@ -25,4 +25,29 @@ feature "from the items index" do
     expect(Item.count).to eq(1)
     expect(page).to have_content("NewGrossCouch")
   end
+
+  scenario "an admin can create an item without specifying image" do
+    admin   = create(:user, role: 1)
+    category = create(:category)
+
+    allow_any_instance_of(ApplicationController).to receive(
+    :current_user).and_return(admin)
+
+    visit admin_items_path
+
+    click_on("Create a New Item")
+
+    expect(current_path).to eq(new_admin_item_path)
+    fill_in "item[name]", with: "NewGrossCouch"
+    fill_in "item[description]", with: "Newdescription"
+    fill_in "item[price]", with: 100.0
+
+    page.select "#{category.name}", :from => "item_category_id"
+    click_on("Create")
+
+    expect(current_path).to eq(item_path(Item.first))
+    expect(Item.count).to eq(1)
+    expect(page).to have_content("NewGrossCouch")
+    expect(Item.first.image_path).to eq("garbage.jpg")
+  end
 end
